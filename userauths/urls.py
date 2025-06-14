af5e6f4307f4481ff8fd518ba154c10e61e206cb
kapilshivhare91 <kapilshivhare9111728981@gmail.com>
2025-06-14 17:10:39 +0530

    continuous

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,8 +1,8 @@
-from django.admin import path
+from django.urls import path
 from userauths import views
 
 app_name = "userauths"
 
-url_patterns {
+url_patterns = [
     path("sign-up/", views.register_views , name="sign-up")
-}
+]
--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from userauths.forms import UserRegisterForm
 
 # Create your views here.
 def register_views(request):
-    return render(request, "userauths/sign_up.html")
+    form = UserRegisterForm()
+
+    context = {
+        'form' : form,
+    }
+    return render(request, "userauths/sign_up.html", context )
