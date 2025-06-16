3d96be0845a0747de362b6a4bbd06083d5f4a71d
kapilshivhare91 <kapilshivhare9111728981@gmail.com>
2025-06-16 21:02:03 +0530

    css_files

--- userauths/views.py
+++ userauths/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
 
-# Create your views here.
+# Create your views here.                                             
 def register_view(request):
 
     if request.method == "POST":
-        print("User registered successfully")
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data['username']
+            messages.success(request,f"Hey {username}, your account has created successfully")
+            new_user = authenticate( 
+                username=form.cleaned_data['email'],
+                password=form.cleaned_data['password1']
+            )
+
+            login(request, new_user)
+            return redirect("core:index")
 
     else:
-        print("User registration failed")
+        form = UserRegisterForm()
 
-    form = UserRegisterForm()
+
 
     context = {
         'form' : form,
