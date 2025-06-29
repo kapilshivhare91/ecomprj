b018cae137987a4e2070875fcdc450dc455cac8f
kapilshivhare91 <kapilshivhare9111728981@gmail.com>
2025-06-29 23:29:55 +0530

    database_structure_1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+class category(models.Model):
+    cid = ShortUUIDField(unique=True, Length=10, max_length=20, prefix='cat', alphabet='abcdefgh1234')
+    title = models.CharField(max_length=100)
+    image = models.Imagefield(upload_to="category")
+
+    class Meta:
+        verbose_name_plural = "categories"
+        
+
